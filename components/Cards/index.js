4a4eb25a8c689ac34dbffb4683f2cd51135d4555
mpaolodr <mpaolodr_1504@yahoo.com>
2019-12-06 12:00:22 -0500

    finished card component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,50 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function createArticle(obj) {
+  //CREATE ELEMENTS
+  const cardContainer = document.createElement("div");
+  const headLine = document.createElement("div");
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const authorName = document.createElement("span");
+
+  //SETUP CLASSES
+  cardContainer.classList.add("card");
+  headLine.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+
+  //SETUP ATTRIBUTES
+  img.src = obj.authorPhoto;
+
+  //SETUP CONTENT
+  headLine.textContent = obj.headline;
+  authorName.textContent = obj.authorName;
+
+  //APPEND
+  cardContainer.appendChild(headLine);
+  cardContainer.appendChild(author);
+  author.appendChild(imgContainer);
+  author.appendChild(authorName);
+  imgContainer.appendChild(img);
+
+  return cardContainer;
+}
+
+const cards = document.querySelector(".cards-container");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    Object.keys(response.data.articles).forEach(key => {
+      response.data.articles[`${key}`].forEach(article => {
+        cards.appendChild(createArticle(article));
+      });
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
