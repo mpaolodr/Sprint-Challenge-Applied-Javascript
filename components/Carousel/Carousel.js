274149fbf41a0c2c6774133ca1d1b24dbbd956f7
mpaolodr <mpaolodr_1504@yahoo.com>
2019-12-07 13:36:57 -0500

    edited some css styles and refactored js code for carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -18,29 +18,17 @@
   </div>
 */
 
-function createCarousel() {
+function createCarousel(...imgs) {
   //CREATE ELEMENTS
   const carousel = document.createElement("div");
   const leftBtn = document.createElement("div");
-  const imgOne = document.createElement("img");
-  const imgTwo = document.createElement("img");
-  const imgThree = document.createElement("img");
-  const imgFour = document.createElement("img");
   const rightBtn = document.createElement("div");
+  const imgArr = [...imgs];
 
   //SETUP CLASSES
   carousel.classList.add("carousel");
   leftBtn.classList.add("left-button");
   rightBtn.classList.add("right-button");
-  imgTwo.classList.add("hide");
-  imgThree.classList.add("hide");
-  imgFour.classList.add("hide");
-
-  //SETUP ATTRIBUTES
-  imgOne.setAttribute("src", "../../assets/carousel/computer.jpeg");
-  imgTwo.setAttribute("src", "../../assets/carousel/mountains.jpeg");
-  imgThree.setAttribute("src", "../../assets/carousel/trees.jpeg");
-  imgFour.setAttribute("src", "../../assets/carousel/turntable.jpeg");
 
   //SETUP CONTENT
   leftBtn.textContent = "<";
@@ -48,83 +36,68 @@ function createCarousel() {
 
   //APPEND
   carousel.appendChild(leftBtn);
-  carousel.appendChild(imgOne);
-  carousel.appendChild(imgTwo);
-  carousel.appendChild(imgThree);
-  carousel.appendChild(imgFour);
   carousel.appendChild(rightBtn);
+  imgArr.forEach(image => {
+    const img = document.createElement("img");
+    img.src = `${image}`;
+    carousel.appendChild(img);
+  });
+
+  let counter = 0;
 
   //ADD EVENT LISTENER TO BUTTONS
-  //I WILL TRY LATER TO KIND OF JUST LOOP THROUGH AN ARRAY OF IMAGES AND THEN JUST DISPLAY THEM AFTER BTN CLICK
-  //I JUST REALIZED THIS NOW!
   rightBtn.addEventListener("click", function(e) {
-    if (
-      imgTwo.className === "hide" &&
-      imgThree.className === "hide" &&
-      imgFour.className === "hide"
-    ) {
-      imgOne.classList.add("hide");
-      imgTwo.classList.toggle("hide");
-    } else if (
-      imgOne.className === "hide" &&
-      imgThree.className === "hide" &&
-      imgFour.className === "hide"
-    ) {
-      imgTwo.classList.toggle("hide");
-      imgThree.classList.toggle("hide");
-    } else if (
-      imgOne.className === "hide" &&
-      imgTwo.className === "hide" &&
-      imgFour.className === "hide"
-    ) {
-      imgThree.classList.add("hide");
-      imgFour.classList.remove("hide");
-    } else if (
-      imgOne.className === "hide" &&
-      imgTwo.className === "hide" &&
-      imgThree.className === "hide"
-    ) {
-      imgOne.classList.toggle("hide");
-      imgFour.classList.toggle("hide");
+    if (counter < imgArr.length - 1) {
+      carousel.style.transition = "transform 0.5s ease-in-out";
+      counter++;
+      carousel.style.transform =
+        "translateX(" + -e.target.parentElement.clientWidth * counter + "px)";
+      rightBtn.style.transform =
+        "translateX(" + e.target.parentElement.clientWidth * counter + "px)";
+      leftBtn.style.transform =
+        "translateX(" + e.target.parentElement.clientWidth * counter + "px)";
+    } else if (counter >= imgArr.length - 1) {
+      counter = 0;
+      carousel.style.transform =
+        "translateX(" + -e.target.parentElement.clientWidth * counter + "px)";
+      rightBtn.style.transform =
+        "translateX(" + e.target.parentElement.clientWidth * counter + "px)";
+      leftBtn.style.transform =
+        "translateX(" + e.target.parentElement.clientWidth * counter + "px)";
     }
-    e.stopPropagation();
   });
 
   leftBtn.addEventListener("click", function(e) {
-    if (
-      imgTwo.className === "hide" &&
-      imgThree.className === "hide" &&
-      imgFour.className === "hide"
-    ) {
-      imgOne.classList.add("hide");
-      imgFour.classList.toggle("hide");
-    } else if (
-      imgOne.className === "hide" &&
-      imgThree.className === "hide" &&
-      imgFour.className === "hide"
-    ) {
-      imgOne.classList.toggle("hide");
-      imgTwo.classList.toggle("hide");
-    } else if (
-      imgOne.className === "hide" &&
-      imgTwo.className === "hide" &&
-      imgFour.className === "hide"
-    ) {
-      imgThree.classList.toggle("hide");
-      imgTwo.classList.toggle("hide");
-    } else if (
-      imgOne.className === "hide" &&
-      imgTwo.className === "hide" &&
-      imgThree.className === "hide"
-    ) {
-      imgFour.classList.toggle("hide");
-      imgThree.classList.toggle("hide");
+    if (counter > 0) {
+      carousel.style.transition = "transform 0.5s ease-in-out";
+      counter--;
+      carousel.style.transform =
+        "translateX(" + -e.target.parentElement.clientWidth * counter + "px)";
+      rightBtn.style.transform =
+        "translateX(" + e.target.parentElement.clientWidth * counter + "px)";
+      leftBtn.style.transform =
+        "translateX(" + e.target.parentElement.clientWidth * counter + "px)";
+    } else if (counter <= 0) {
+      counter = imgArr.length - 1;
+      carousel.style.transform =
+        "translateX(" + -e.target.parentElement.clientWidth * counter + "px)";
+      rightBtn.style.transform =
+        "translateX(" + e.target.parentElement.clientWidth * counter + "px)";
+      leftBtn.style.transform =
+        "translateX(" + e.target.parentElement.clientWidth * counter + "px)";
     }
-    e.stopPropagation();
   });
 
   return carousel;
 }
 
 const carouselCont = document.querySelector(".carousel-container");
-carouselCont.appendChild(createCarousel());
+carouselCont.style.overflow = "hidden";
+carouselCont.appendChild(
+  createCarousel(
+    "../../assets/carousel/computer.jpeg",
+    "../../assets/carousel/mountains.jpeg",
+    "../../assets/carousel/trees.jpeg",
+    "../../assets/carousel/turntable.jpeg"
+  )
+);
